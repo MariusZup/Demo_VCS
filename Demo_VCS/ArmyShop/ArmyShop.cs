9b275254bc30eaee264ea5c38afaa54b0a3d5ffe
lodarius <dexxa55@gmail.com>
2022-07-03 23:51:20 +0300

    test form

---
+++ Demo_VCS/ArmyShop/ArmyShop.cs
@@ -0,0 +1,41 @@
+﻿using Framework;
+using Framework.Pages.ArmyShop;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_VCS.ArmyShop
+{
+    public class ArmyShop
+    {
+
+        [SetUp]
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://www.army-shop.lt/");
+        }
+
+        [Test]
+
+        public static void Blogas()
+        {
+            string expectedMessage = "CARINTHIA - sukurta Jūsų saugumui ir komfortui";
+            string acctualMessage;
+
+            ArmyShopPage.clickBlogas();
+            acctualMessage = ArmyShopPage.readMessage();
+
+            Assert.AreEqual(expectedMessage, acctualMessage);
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
---
+++ Framework/Pages/ArmyShop/ArmyShopPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Pages.ArmyShop
+{
+    public class ArmyShopPage
+    {
+        public static void closeAdvertisment()
+        {
+            string locator = "//*[@id='sbox - btn - close']";
+            Common.waitForElementToBeVisible(locator);
+            Common.clickElement(locator);
+        }
+
+        public static void Blogas()
+        {
+            string locator = "//*[@id='position_menu1']/div/div/div/div/ul/li[5]/a/span";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessage()
+        {
+            string locator = "//*[@id='content']/div[1]/div[1]/div/div[1]/div[1]/a";
+            return Common.getElementText(locator);
+        }
+
+        public static void clickBlogas()
+        {
+            string locator = "//*[@id='position_menu1']/div/div/div/div/ul/li[5]/a/span";
+            Common.clickElement(locator);
+        }
+    }
+}
